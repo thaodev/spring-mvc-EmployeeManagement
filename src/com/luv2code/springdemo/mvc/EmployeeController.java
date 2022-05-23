822293b4b8dd329687514a6d3deeda07e57d099c
Thao Nguyen <thaontt1408@gmail.com>
2022-05-23 15:44:43 -0600

    Add initial files

---
+++ src/com/luv2code/springdemo/mvc/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmployeeController {
+	//need a controller method to show the initial HTML form
+	
+	//need a controller method to process the HTML form
+	
+}
