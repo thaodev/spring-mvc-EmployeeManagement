6ad17c22963c09fb28749348a153691ac58ec4b3
Thao Nguyen <thaontt1408@gmail.com>
2022-05-23 21:29:32 -0600

    create package for DAO and DAOImpl files

---
+++ src/com/luv2code/springdemo/dao/EmployeeDAO.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import com.luv2code.springdemo.entity.Employee;
+
+public interface EmployeeDAO {
+	
+	public List<Employee> getEmployees();
+}
---
+++ src/com/luv2code/springdemo/dao/EmployeeDAOImpl.java
@@ -0,0 +1,35 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.Employee;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	//need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Employee> getEmployees() {
+		
+		//get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		//create a query
+		Query<Employee> theQuery = currentSession.createQuery("from Employee", Employee.class);
+		
+		//execute query and get result list
+		List<Employee> employees = theQuery.getResultList();
+		
+		return employees;
+	}
+
+}
