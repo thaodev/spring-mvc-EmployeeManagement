3eda5f105e3b24ffe1998dddcc3d33bbbc039f3a
Thao Nguyen <thaontt1408@gmail.com>
2022-05-23 21:21:12 -0600

    Update the package name from mvc to controller

--- src/com/luv2code/springdemo/controller/HomeController.java
+++ src/com/luv2code/springdemo/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.luv2code.springdemo.mvc;
+package com.luv2code.springdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
