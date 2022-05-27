2de7a3ff667e0a838bf5bb3147f485ec7c6a0b34
Thao Nguyen <thaontt1408@gmail.com>
2022-05-27 14:20:31 -0600

    Add service layer, move the @Transactional over to the method inside the service impl

---
+++ src/com/luv2code/springdemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,26 @@
+package com.luv2code.springdemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.dao.EmployeeDAO;
+import com.luv2code.springdemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	//Service depends on DAO => Inject the dependency
+	@Autowired
+	private EmployeeDAO employeeDAO;
+	
+	
+	@Override
+	@Transactional
+	public List<Employee> getEmployees() {
+		return employeeDAO.getEmployees();
+	}
+
+}
